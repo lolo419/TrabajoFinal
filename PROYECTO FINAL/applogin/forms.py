eb21e08974eee63b1ad9431bea1f6aa8e244ae6c
lolo419 <136233702+lolo419@users.noreply.github.com>
2024-09-03 18:46:26 -0300

    Delete PROYECTO FINAL/applogin directory

--- PROYECTO FINAL/applogin/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.db import models
-class Notas(models.Model):
-    cuerpo = models.TextField(max_length=360)
-    autor = models.CharField(max_length=30)
-    areUHappy = models.BooleanField()
